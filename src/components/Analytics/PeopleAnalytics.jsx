a18e370a3e7074170bc89808fc79b2ef72eda9d3
JigarRQe <jigarrathod.ecommerce@gmail.com>
2024-09-11 11:25:54 +0530

    remove order box

--- src/components/Analytics/PeopleAnalytics.jsx
+++ src/components/Analytics/PeopleAnalytics.jsx
@@ -534,7 +534,7 @@ const PeopleAnalytics = () => {
                 </button>
               </div>
 
-              <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+              {/* <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
                 {orderSalesData.map((orderSaleTitle, key) => (
                   <div
                     key={key}
@@ -548,7 +548,7 @@ const PeopleAnalytics = () => {
                     </div>
                   </div>
                 ))}
-              </div>
+              </div> */}
               <div>
                 <div className="bg-green-300 p-5 rounded-md shadow-md mt-5">
                   <p className="text-xl font-bold text-black mb-2">
