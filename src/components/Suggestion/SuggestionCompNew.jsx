d6dae7d2279cbc42a36819405f3ce1e32bc41464
Harshil Solanki <harshilsolanki.ace@gmail.com>
2024-09-30 17:05:28 +0530

    Implemented dynamic tab functionality

--- src/components/Suggestion/SuggestionCompNew.jsx
+++ src/components/Suggestion/SuggestionCompNew.jsx
@@ -50,14 +50,7 @@ const SuggestionCompNew = () => {
     const [problemStatement, setProblemStatement] = useState({});
     const [suggestionId, setSuggestionId] = useState(null);
     const [manageAccordions, setManageAccordions] = useState([]);
-    const [accordionTab, setAccordionTab] = useState({
-        'customer': activeTabObj,
-        'order': hiddenTabObj,
-        'product': hiddenTabObj,
-        'top_selling_product': hiddenTabObj,
-        'top_abandoned_product': hiddenTabObj,
-        'product_recommendations': hiddenTabObj,
-    });
+    const [accordionTab, setAccordionTab] = useState({});
     const [discountObj, setDiscountObj] = useState({});
     const [updatedDiscountObj, setUpdatedDiscountObj] = useState({});
     const [inputTypeValue, setInputTypeValue] = useState(0);
@@ -77,7 +70,12 @@ const SuggestionCompNew = () => {
                     const innerAccordions = [];
                     result.data?.tblLevel2?.map((tblLevel) => {
                         initialAccordions.push(collapsedAccordionState);
-                        return tblLevel?.suggestion?.data?.map(() => {
+                        return tblLevel?.suggestion?.data?.map((items) => {
+                            Object.keys(items).map((item) => {
+                                const defaultClass = (item == 'Customer_ID' || item == 'Customer_IP' || item == 'Days_After_Onboarding') ? activeTabObj : hiddenTabObj;
+                                setAccordionTab(prev => ({ ...prev, [item]: defaultClass }));
+                                return true;
+                            })
                             return innerAccordions.push(defaultInnerAccordion)
                         })
                     });
@@ -128,7 +126,9 @@ const SuggestionCompNew = () => {
     );
 
     const handleAccordionTab = (type) => {
-        const tabTypes = ['customer', 'order', 'product', 'top_selling_product', 'top_abandoned_product', 'product_recommendations'];
+        const tabTypes = Object.keys(accordionTab).map((tab) => {
+            return tab;
+        });
         const newAccordionState = tabTypes.reduce((acc, tab) => {
             acc[tab] = (tab === type) ? activeTabObj : hiddenTabObj;
             return acc;
@@ -145,39 +145,29 @@ const SuggestionCompNew = () => {
             return item.plus === "hidden" ? defaultInnerAccordion : collapsedInnerAccordion;
         })
         setPlusMinus(newPlusMinus);
-        setAccordionTab({
-            'customer': activeTabObj,
-            'order': hiddenTabObj,
-            'product': hiddenTabObj,
-            'top_selling_product': hiddenTabObj,
-            'top_abandoned_product': hiddenTabObj,
-            'product_recommendations': hiddenTabObj,
+        setAccordionTab(prevAccordionTab => {
+            const updatedAccordionTab = Object.keys(prevAccordionTab).reduce((acc, key) => {
+                acc[key] = hiddenTabObj;
+                return acc;
+            }, {});
+
+            updatedAccordionTab['Customer_ID'] = activeTabObj;
+            updatedAccordionTab['Customer_IP'] = activeTabObj;
+            updatedAccordionTab['Days_After_Onboarding'] = activeTabObj;
+
+            return updatedAccordionTab;
         });
     }
 
-    const renderAccordionTabs = (dataItem, accordionTab, accordionTabClass, handleAccordionTab) => {
-        const keyMappings = {
-            'Products': { type: 'product', title: 'Product' },
-            'Orders': { type: 'order', title: 'Order' },
-            'Customer_ID': { type: 'customer', title: 'Customer' },
-            'Customer_IP': { type: 'customer', title: 'Customer' },
-            'Top_Selling_Products': { type: 'top_selling_product', title: 'Top Selling Product' },
-            'Top_Abandoned_Products': { type: 'top_abandoned_product', title: 'Top Abandoned Product' },
-            'product_recommendations': { type: 'product_recommendations', title: 'Product Recommendations' },
-        };
-
+    const renderAccordionTabs = (dataItem) => {
         return Object.keys(dataItem).map((key, index) => {
-            const { type, title } = keyMappings[key] || {};
-
-            if (!type || !title) return null; // Skip if the key is not in the mapping
-
             return (
                 <a
                     key={index}
-                    className={`${accordionTabClass} ${accordionTab?.[type]?.active_tab}`}
-                    onClick={() => handleAccordionTab(type)}
+                    className={`${accordionTabClass} ${accordionTab?.[key]?.active_tab}`}
+                    onClick={() => handleAccordionTab(key)}
                 >
-                    {title}
+                    {key}
                 </a>
             );
         });
@@ -204,26 +194,27 @@ const SuggestionCompNew = () => {
         </div>
     );
 
-    const renderAccordionContent = (dataItem, accordionTab) => (
+    const renderAccordionContent = (dataItem) => (
         <div>
-            <div className={`leading-relaxed ${accordionTab?.customer?.active_tab_body}`}>
-                {dataItem.Customer_ID || dataItem.Customer_IP}
-            </div>
-            <div className={`leading-relaxed ${accordionTab?.order?.active_tab_body}`}>
-                {dataItem.Orders}
-            </div>
-            <div className={`leading-relaxed ${accordionTab?.product?.active_tab_body}`}>
-                {dataItem?.Products?.map(renderProduct)}
-            </div>
-            <div className={`leading-relaxed ${accordionTab?.top_selling_product?.active_tab_body}`}>
-                {dataItem?.Top_Selling_Products?.map(renderProduct)}
-            </div>
-            <div className={`leading-relaxed ${accordionTab?.top_abandoned_product?.active_tab_body}`}>
-                {dataItem?.Top_Abandoned_Products?.map(renderProduct)}
-            </div>
-            <div className={`leading-relaxed ${accordionTab?.product_recommendations?.active_tab_body}`}>
-                {dataItem?.product_recommendations?.map(renderProduct)}
-            </div>
+            {
+                Object.keys(accordionTab).map((tab, index) => {
+                    if (!dataItem[tab]) return null;
+
+                    if (Array.isArray(dataItem[tab])) {
+                        return (
+                            <div key={index} className={`leading-relaxed ${accordionTab?.[tab]?.active_tab_body}`}>
+                                {dataItem[tab].map(renderProduct)}
+                            </div>
+                        );
+                    }
+
+                    return (
+                        <div key={index} className={`leading-relaxed ${accordionTab?.[tab]?.active_tab_body}`}>
+                            {dataItem[tab]}
+                        </div>
+                    );
+                })
+            }
         </div>
     );
 
@@ -248,7 +239,7 @@ const SuggestionCompNew = () => {
                                 <div key={i} className={`mt-5 ml-16.5 duration-200 ease-in-out ${manageAccordions?.[index]?.content}`}>
                                     <div className="rounded-md border border-stroke p-4 shadow-9 dark:border-strokedark dark:shadow-none md:p-6 xl:p-7.5">
                                         <button className="flex w-full items-center justify-between gap-2 " onClick={() => handlePlusMinus(i)}>
-                                            {dataItem.Customer_ID || dataItem.Customer_IP}
+                                            {dataItem.Customer_ID || dataItem.Customer_IP || dataItem.Days_After_Onboarding}
                                             <div className="flex h-9 w-full max-w-9 items-center justify-center rounded-full border border-primary dark:border-white">
                                                 <PlusSvg plusMinus={plusMinus?.[i]} />
                                                 <MinusSvg plusMinus={plusMinus?.[i]} />
@@ -258,11 +249,11 @@ const SuggestionCompNew = () => {
                                             <div className="rounded-sm">
                                                 <div className="mb-6 flex flex-wrap gap-5 border-b border-stroke dark:border-strokedark sm:gap-10">
                                                     {
-                                                        renderAccordionTabs(dataItem, accordionTab, accordionTabClass, handleAccordionTab)
+                                                        renderAccordionTabs(dataItem)
                                                     }
                                                 </div>
                                                 {
-                                                    renderAccordionContent(dataItem, accordionTab)
+                                                    renderAccordionContent(dataItem)
                                                 }
                                             </div>
                                         </div>
@@ -318,14 +309,6 @@ const SuggestionCompNew = () => {
     }
 
     const toggleAccordionState = (currentState, defaultState) => {
-        setAccordionTab({
-            'customer': activeTabObj,
-            'order': hiddenTabObj,
-            'product': hiddenTabObj,
-            'top_selling_product': hiddenTabObj,
-            'top_abandoned_product': hiddenTabObj,
-            'product_recommendations': hiddenTabObj,
-        });
         const isCollapsed = JSON.stringify(currentState) === JSON.stringify(defaultState);
         return isCollapsed ? collapsedAccordionState : defaultAccordionState;
     };
@@ -339,6 +322,10 @@ const SuggestionCompNew = () => {
         });
 
         setManageAccordions(updatedAccordions);
+        const newPlusMinus = plusMinus.map(() => {
+            return defaultInnerAccordion;
+        })
+        setPlusMinus(newPlusMinus);
     };
 
     const modifyStringWithPercentage = (discountObj) => {
