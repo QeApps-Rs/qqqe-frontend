67356b102c3e36cebb24ed319a79fd4265ed0692
Harshil Solanki <harshilsolanki.ace@gmail.com>
2024-10-16 13:08:41 +0530

    Implemented survey question and answer model

--- src/components/Campaigns/CampaignsDetails/SurveyCampaignComponent.jsx
+++ src/components/Campaigns/CampaignsDetails/SurveyCampaignComponent.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Modal from "../../higherOrderComponent/Model/model";
+import AccordionIconSvg from "../../../images/svg-icons/AccordionIcon";
 
 /* eslint-disable react/prop-types */
 const SurveyCampaignCompon = ({ productDetailsData }) => {
@@ -29,7 +32,43 @@ const SurveyCampaignCompon = ({ productDetailsData }) => {
     );
   };
   const surveyState = productDetailsData?.json_response?.survey_controller;
-  console.log(["survey", surveyState]);
+  const defaultSurveyState = surveyState?.survey?.map(() => {
+    return { icon: "rotate-180", content: "hidden" };
+  });
+
+  const [manageAccordions, setManageAccordions] = useState([]);
+  const [surveyDetailModal, setSurveyDetailModal] = useState(false);
+
+  const onModalOpenClose = () => {
+    setSurveyDetailModal(!surveyDetailModal);
+    setManageAccordions(defaultSurveyState);
+  };
+
+  const collapsedAccordionState = {
+    icon: "rotate-180",
+    content: "hidden",
+  };
+  const defaultAccordionState = {
+    icon: "",
+    content: "block",
+  };
+
+  const toggleAccordionState = (currentState, defaultState) => {
+    const isCollapsed =
+      JSON.stringify(currentState) === JSON.stringify(defaultState);
+    return isCollapsed ? collapsedAccordionState : defaultAccordionState;
+  };
+
+  const handleAccordionClick = (clickedIndex) => {
+    const updatedAccordions = manageAccordions?.map((accordion, index) => {
+      if (index === clickedIndex) {
+        return toggleAccordionState(accordion, defaultAccordionState);
+      }
+      return collapsedAccordionState;
+    });
+    setManageAccordions(updatedAccordions);
+  };
+
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default mt-4 p-8">
       <div className="flex justify-between items-center">
@@ -53,7 +92,12 @@ const SurveyCampaignCompon = ({ productDetailsData }) => {
           {surveyState?.survey_type == "survey" && (
             <div className="inline-flex items-center font-bold text-md transform hover:scale-125 transition duration-300 ease-in-out text-blue-700 cursor-pointer hover:text-blue-900 transition duration-500 ease-in-out">
               <i className="fa fa-eye mr-2" aria-hidden="true"></i>
-              <span className="hover:underline">View survey details</span>
+              <span
+                className="hover:underline"
+                onClick={() => onModalOpenClose()}
+              >
+                View survey details
+              </span>
             </div>
           )}
           {surveyState?.survey_type == "button" && (
@@ -78,6 +122,59 @@ const SurveyCampaignCompon = ({ productDetailsData }) => {
           )}
         </div>
       </div>
+      {surveyDetailModal && (
+        <Modal
+          isOpen={surveyDetailModal}
+          onClose={() => onModalOpenClose()}
+          onClickInChild=""
+          body={
+            <>
+              <div className="block">
+                {manageAccordions?.map((accordion, index) => (
+                  <div
+                    key={index}
+                    className="rounded-lg border border-bodydark2  p-5 shadow-lg transition-transform duration-300 ease-in-out hover:shadow-xl w-full my-4 "
+                  >
+                    <button
+                      className="flex w-full items-center gap-3"
+                      onClick={() => handleAccordionClick(index)}
+                    >
+                      <div
+                        className={`flex h-12 w-12 items-center justify-center rounded-full bg-gray-3 dark:bg-blue-800 transform transition-transform duration-300 ${accordion.icon}`}
+                      >
+                        <AccordionIconSvg classRotate={accordion.icon} />
+                      </div>
+                      <div>
+                        <h4 className="text-left font-semibold text-customGray dark:text-blue-200 text-lg">
+                          {surveyState?.survey[index].question}
+                        </h4>
+                      </div>
+                    </button>
+                    <div
+                      className={`mt-4 transition-all duration-500 ease-in-out ${accordion.content}`}
+                    >
+                      <ul className="pl-8 list-disc text-customGray dark:text-blue-300">
+                        {surveyState?.survey[index]?.answers?.map(
+                          (answer, key) => (
+                            <li key={key} className="my-2 text-base">
+                              {answer}
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          }
+          btnClose="Close"
+          btnSubmit=""
+          mdlTitle="Survey Details"
+          showFooter={false}
+          isAnalytics={true}
+        />
+      )}
     </div>
   );
 };
